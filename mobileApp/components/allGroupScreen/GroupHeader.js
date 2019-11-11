8c26da7212fbbc75f2019cbe1009b4d11071bf5d
Ken Guan <21guanr@berkeley.edu>
2019-11-11 12:21:02 -0800

    use 2-space indent for custom components

--- mobileApp/components/allGroupScreen/GroupHeader.js
+++ mobileApp/components/allGroupScreen/GroupHeader.js
@@ -3,63 +3,63 @@ import { View, Text, Image, StyleSheet, } from 'react-native';
 
 class GroupHeader extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.username = props.username;
-    };
+  constructor(props) {
+    super(props);
+    this.username = props.username;
+  };
 
-    render () {
-        return (
-            <View style={viewStyle}>
-                <View style={styles.headerStyle}>
-                    <View style={{marginLeft:-20}}>
-                        <Image style={styles.imagestyle} source={require('../../assets/images/james.png')}/>
-                    </View>
-                    <View style={{marginLeft: 20,}}>
-                        <Text style={styles.nameText}> @James-666 </Text>
-                        <Text style={styles.oweText}> You owe $300 {'\n'} You are owed $100 </Text>
-                    </View>
-                </View>
-            </View>
-        );
-    };
-};
+  render() {
+    return (
+      <View style={viewStyle}>
+        <View style={styles.headerStyle}>
+          <View style={{ marginLeft: -20 }}>
+            <Image style={styles.imagestyle} source={require('../../assets/images/james.png')} />
+          </View>
+          <View style={{ marginLeft: 20, }}>
+            <Text style={styles.nameText}> @James-666 </Text>
+            <Text style={styles.oweText}> You owe $300 {'\n'} You are owed $100 </Text>
+          </View>
+        </View>
+      </View>
+    );
+  };
+}
 
 const styles = StyleSheet.create({
-    headerStyle: {
-        flexDirection:'row', 
-        alignItems: 'flex-start', 
-        marginTop:90,
-        backgroundColor: '#DFE8ED',
-    },
-    imagestyle: {
-        width:100,
-        height:100,
-        borderRadius: 100/2,
-    },
-    nameText: {
-        color:'#00597D', 
-        fontSize:22, 
-        fontWeight: "bold",
-        paddingBottom: 10,
-        paddingTop: 20,
-    },
-    oweText: {
-        color: '#859EAC',
-        fontSize: 12,
-    },
+  headerStyle: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    marginTop: 90,
+    backgroundColor: '#DFE8ED',
+  },
+  imagestyle: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+  },
+  nameText: {
+    color: '#00597D',
+    fontSize: 22,
+    fontWeight: "bold",
+    paddingBottom: 10,
+    paddingTop: 20,
+  },
+  oweText: {
+    color: '#859EAC',
+    fontSize: 12,
+  },
 })
 
 const viewStyle = {
-    width: '100%',
-    height: '30%',
-    backgroundColor: '#DFE8ED',
-    justifyContent: 'flex-start', 
-    alignItems: 'center',
+  width: '100%',
+  height: '30%',
+  backgroundColor: '#DFE8ED',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
 };
 
 const textStyle = {
-    fontSize: 17,
+  fontSize: 17,
 };
 
 export default GroupHeader;
--- mobileApp/components/singleGroupScreen/EventDialogue.js
+++ mobileApp/components/singleGroupScreen/EventDialogue.js
@@ -7,107 +7,107 @@ import EventDetail from './EventDetail';
 const author = 'ken';
 
 const getEventsFromIds = (ids) => {
-    return ids.map(id => ({
-        id,
-        name: `Event ${id}`,
-        time: '2019-10-26T22:14:35Z',
-        location: 'Cal Hacks 6.0',
-        paymentInfo: {
-            ken: id,
-        },
-        users: ['ken', 'sprite'],
-        author,
-    }));
+  return ids.map(id => ({
+    id,
+    name: `Event ${id}`,
+    time: '2019-10-26T22:14:35Z',
+    location: 'Cal Hacks 6.0',
+    paymentInfo: {
+      ken: id,
+    },
+    users: ['ken', 'sprite'],
+    author,
+  }));
 };
 
 class EventDialogue extends React.Component {
 
-    constructor (props) {
-        super(props);
-        this.state = {
-            touchedEvent: null,
-        };
-        this.user = props.user;
-        this.eventIds = props.eventIds;
-        this.eventsData = getEventsFromIds(this.eventIds);
+  constructor(props) {
+    super(props);
+    this.state = {
+      touchedEvent: null,
     };
+    this.user = props.user;
+    this.eventIds = props.eventIds;
+    this.eventsData = getEventsFromIds(this.eventIds);
+  };
 
-    render () {
-        return (
-            <ScrollView style={background}>
-                {
-                    this.eventsData.map(event => 
-                        <EventPreviewContainer
-                            id={event.id}
-                            user={this.user}
-                            key={event.id}
-                            name={event.name}
-                            time={event.time}
-                            location={event.location}
-                            payment={event.paymentInfo}
-                            users={event.users}
-                            author={event.author}
-                            setParentState={(state) => this.setState(state)}
-                        ></EventPreviewContainer>
-                    )
-                }
-                <Overlay
-                    isVisible={!!this.state.touchedEvent}
-                    windowBackgroundColor="rgba(255, 255, 255, .3)"
-                    overlayBackgroundColor="rgb(255, 255, 255)"
-                    onBackdropPress={() => { this.setState({ touchedEvent: false }) }}
-                    width="84%"
-                    height="60%"
-                >
-                    <EventDetail id={this.state.touchedEvent} user={this.user} > </EventDetail>
-                </Overlay>
-            </ScrollView>
-        );
-    };
+  render() {
+    return (
+      <ScrollView style={background}>
+        {
+          this.eventsData.map(event =>
+            <EventPreviewContainer
+              id={event.id}
+              user={this.user}
+              key={event.id}
+              name={event.name}
+              time={event.time}
+              location={event.location}
+              payment={event.paymentInfo}
+              users={event.users}
+              author={event.author}
+              setParentState={(state) => this.setState(state)}
+            ></EventPreviewContainer>
+          )
+        }
+        <Overlay
+          isVisible={!!this.state.touchedEvent}
+          windowBackgroundColor="rgba(255, 255, 255, .3)"
+          overlayBackgroundColor="rgb(255, 255, 255)"
+          onBackdropPress={() => { this.setState({ touchedEvent: false }) }}
+          width="84%"
+          height="60%"
+        >
+          <EventDetail id={this.state.touchedEvent} user={this.user} > </EventDetail>
+        </Overlay>
+      </ScrollView>
+    );
+  };
 }
 
 class EventPreviewContainer extends React.Component {
 
-    constructor (props) {
-        super(props);
-        this.id = props.id;
-        this.user = props.user;
-        this.name = props.name;
-        this.time = props.time;
-        this.location = props.location;
-        this.payment = props.payment;
-        this.users = props.users;
-        this.author = props.author;
-    };
+  constructor(props) {
+    super(props);
+    this.id = props.id;
+    this.user = props.user;
+    this.name = props.name;
+    this.time = props.time;
+    this.location = props.location;
+    this.payment = props.payment;
+    this.users = props.users;
+    this.author = props.author;
+  };
 
-    render () {
-        return (
-            <View style={container}>
-                <TouchableOpacity onPress={() => this.props.setParentState({ touchedEvent: this.id })}>
-                    <EventPreview
-                        user={this.user}
-                        name={this.name}
-                        time={this.time}
-                        location={this.location}
-                        payment={this.payment[this.user] ? this.payment[this.user] : 0}
-                        users={this.users}
-                        author={this.author}
-                    ></EventPreview>
-                </TouchableOpacity>
-            </View>
-        );
-    };
+  render() {
+    return (
+      <View style={container}>
+        <TouchableOpacity onPress={() => this.props.setParentState({ touchedEvent: this.id })}>
+          <EventPreview
+            user={this.user}
+            name={this.name}
+            time={this.time}
+            location={this.location}
+            payment={this.payment[this.user] ? this.payment[this.user] : 0}
+            users={this.users}
+            author={this.author}
+          ></EventPreview>
+        </TouchableOpacity>
+      </View>
+    );
+  };
 }
 
 const background = {
-    flex: 1,
-    backgroundColor: '#fff',
+  flex: 1,
+  backgroundColor: '#fff',
 };
 
 const container = {
-    flexDirection: 'column',
-    alignItems: 'stretch',
-    marginTop: 8,
+  flexDirection: 'column',
+  alignItems: 'stretch',
+  marginTop: 8,
 };
 
 export default EventDialogue;
