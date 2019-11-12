8da7b3de38ec959b60dc2c8711ad6c329c54bc08
Ken Guan <21guanr@berkeley.edu>
2019-11-12 13:50:37 -0800

    MA-27 support existing user login

--- mobileApp/apis/User.js
+++ mobileApp/apis/User.js
@@ -3,28 +3,26 @@ import { LOCAL_IP } from 'react-native-dotenv';
 const baseUrl = `http://${LOCAL_IP}:8080/user`;
 const axios = require('axios');
 
+const mapData = (data) => data ? {
+    username: data.name,
+    userId: data.userid,
+} : null;
+
 exports.allUsers = () => {
     return axios.get(`${baseUrl}/all`)
         .then(res => res.data, 
             err => ['Cannot find at ' + baseUrl]);
-}
+};
 
 exports.login = (username, password) => {
-    mockNameToId = (username) => username === 'ken' ? 1 : 2;
-    const defaultUsername = 'DEFAULT';
-    if (username) {
-        return {
-            username,
-            userid: mockNameToId(username),
-        };
+    if (username === '') {
+        return Promise.resolve({ username: 'DEFAULT', userId: 0 });
     }
-    return {
-        username: defaultUsername,
-        userid: 0,
-    };
+    return axios.get(`${baseUrl}/login?username=${username}&password=${password}`)
+        .then(res => mapData(res.data));
 };
 
 exports.createNewUser = (email, username, password) => {
-    return axios.put(baseUrl, { email, name: username })
-        .then(res => res.data);
+    return axios.put(baseUrl, { email, name: username, password })
+        .then(res => mapData(res.data));
 };
